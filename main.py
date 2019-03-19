1f718cb18d9bde1a98ea7eac5c1e68a3395e8389
Aditya Diwakar <i@adityadiwakar.me>
2019-03-19 13:33:47 -0400

    add block class

---
+++ main.py
@@ -0,0 +1,9 @@
+class Block:
+    def __init__(x, width, velocity):
+        self.x = x
+        self.width = width
+        self.velocty = velocity
+    
+    def move():
+        self.x += velocity
+
